3d189206f9046a28d70e21d4d58d10171943b52e
LiterallyMe-kun <sknoah2002@gmail.com>
2025-01-18 08:07:30 -0700

    Actually done lol

--- Program.cs
+++ Program.cs
@@ -1,7 +1,66 @@
-﻿internal class Program
+﻿using Mission2;
+
+internal class Program
 {
+
+    /*  
+        
+        
+        int numRolls
+        
+    method to divide each index of diceResult[] by numRolls
+
+    post histogram of *'s to demonstrate the percentage each number took
+
+    */
     private static void Main(string[] args)
     {
-        Console.WriteLine("Hello, World!");
+        int[] calculatedRolls;
+        
+        //this has to be a double because Math.round really wanted to be sure if it were a double or decimal
+        int numRolls = 0;
+
+        double rolledFreq = 0;
+
+        
+
+        Required rq = new Required();
+
+        System.Console.WriteLine("How many dice rolls will you throw? Enter a whole numeral (ie. 32):");
+        numRolls = int.Parse(System.Console.ReadLine());
+
+        System.Console.WriteLine("At " + numRolls + "rolls, here are your results!");
+
+        System.Console.WriteLine("{One asterisk equal 1% of 100% of the results)");
+
+        //use the second class on Required.cs, assigning its output (times a certain number was rolled) to a new array
+        calculatedRolls = rq.RollDice(numRolls); 
+
+
+        for (int i = 0; i < calculatedRolls.Length; i++)
+
+        {
+            //reset the asterisk count for each number 
+            string asteriskNum = "";
+            rolledFreq = (calculatedRolls[i] / numRolls);
+
+            rolledFreq = Math.Round(rolledFreq);
+
+            //loop to add asterisks
+            for (int j = 0; j <= calculatedRolls[i]; j++) 
+            {
+                asteriskNum = asteriskNum + "*";
+            }
+
+            System.Console.WriteLine("Percentage of " + (i + 2) + "'s rolled: " + asteriskNum);
+
+        }
+
+
+        System.Console.WriteLine("Thanks for playing!");
     }
+
+
+
+
 }
---
+++ Required.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Mission2
+{
+    /* 
+      method loop to roll dice for numRolls
+
+      2 6-sided dice
+        if statement to increment each index from 0 to 10, 
+                                    since it rolls from 2 to 12
+            int[] diceRecord
+            
+            if diceResult = 2
+                diceRecord[0] = diceRecord[0] +      
+     */
+
+    internal class Required
+    {
+        // output is an array of ints
+        public int[] RollDice(int numRolls) 
+        {
+            int[] diceRecord;
+
+            diceRecord = new int[11];
+
+            for (int i = 0; i < numRolls; i++)
+            {
+                // actually roll two 6-sided dice
+                Random rnd = new Random();
+
+                int diceResult = rnd.Next(1,7) + rnd.Next(1,7);
+
+                //switch case because it's cleaner than ifs
+                switch (diceResult)
+                {
+                    case 2:
+                        diceRecord[0] = diceRecord[0] + 1;
+                        break;
+
+                    case 3:
+                        diceRecord[1] = diceRecord[1] + 1;
+                        break;
+
+                    case 4:
+                        diceRecord[2] = diceRecord[2] + 1;
+                        break;
+
+                    case 5:
+                        diceRecord[3] = diceRecord[3] + 1;
+                        break;
+
+                    case 6:
+                        diceRecord[4] = diceRecord[4] + 1;
+                        break;
+
+                    case 7:
+                        diceRecord[5] = diceRecord[5] + 1;
+                        break;
+
+                    case 8:
+                        diceRecord[6] = diceRecord[6] + 1;
+                        break;
+
+                    case 9:
+                        diceRecord[7] = diceRecord[7] + 1;
+                        break;
+
+                    case 10:
+                        diceRecord[8] = diceRecord[8] + 1;
+                        break;
+
+                    case 11:
+                        diceRecord[9] = diceRecord[9] + 1;
+                        break;
+
+                    case 12:
+                        diceRecord[10] = diceRecord[10] + 1;
+                        break;
+                }
+            }
+            // return the array 
+            return diceRecord;
+        }
+    }
+}
